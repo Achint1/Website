c5a41fc4c30bc2a415e3a97c0ab7ee384fe2975a
achint <Achint1>
2021-09-05 16:32:51 +0530

    Updates

---
+++ precache-manifest.6e9a6d8a7f7d30e010333d9f2ea17ade.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "516fde97a75a59e3e78702891ca2e6ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1555d15951a2fca0835f",
+    "url": "/static/css/main.4e794dd0.chunk.css"
+  },
+  {
+    "revision": "ceac80de3f4c72495998",
+    "url": "/static/js/2.c746506b.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.c746506b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1555d15951a2fca0835f",
+    "url": "/static/js/main.10fc45f9.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "e6574c597d17e9547b1b5a841ede4ed4",
+    "url": "/static/media/profile.e6574c59.jpg"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/static/media/resume.9d3da0e2.pdf"
+  }
+]);
